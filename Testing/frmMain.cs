9b58607d3b9c5803db7573c3f5a7b3abcf6c07d6
Sok Rothoudom <rothoudom@forteinsurance.com>
2022-07-08 07:59:33 +0700

    Revert "Add Application to Tray when closing"
    This reverts commit 861b7881208c0518718ea68b83e68098b6177a93.

--- Testing/frmMain.cs
+++ Testing/frmMain.cs
@@ -44,10 +44,6 @@ namespace Testing
             {
                 e.Cancel = true;
                 WindowState = FormWindowState.Minimized;
-                ShowInTaskbar = false;
-                    
-                //e.Cancel = true;
-                //WindowState = FormWindowState.Minimized;
 
                 //Application.Exit();
                 
@@ -750,27 +746,6 @@ namespace Testing
         {
             Forms.frmUserRoleManagement userRoleManagement = new Forms.frmUserRoleManagement();
             userRoleManagement.ShowDialog();
-        }
-
-        private void niIMSTool_MouseDoubleClick(object sender, MouseEventArgs e)
-        {
-            ShowInTaskbar = true;
-            niIMSTool.Visible = false;
-            WindowState = FormWindowState.Maximized;
-        }
-
-        private void frmMain_Resize(object sender, EventArgs e)
-        {
-            if (WindowState == FormWindowState.Minimized)
-            {
-                ShowInTaskbar = true;
-                niIMSTool.Visible = true;
-            }
-        }
-
-        private void quitToolStripMenuItem_Click_1(object sender, EventArgs e)
-        {
-            Application.Exit();
         }
 
         //private List<string> EverythingBetween(string source, string start, string end)
