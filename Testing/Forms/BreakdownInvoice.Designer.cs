105a4e8c8d5bb477fdff7cdc91eab01099f375e7
Southeane1996 <theane@forteinsurance.com>
2022-07-08 08:48:51 +0700

    BreakdownInovoice for Ms Monea

---
+++ Testing/Forms/BreakdownInvoice.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Testing.Forms
+{
+    partial class BreakdownInvoice
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbTitle = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbInvoiceNo = new System.Windows.Forms.TextBox();
+            this.btnPrint = new Testing.cus_button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbTitle
+            // 
+            this.lbTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.lbTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lbTitle.Font = new System.Drawing.Font("Palatino Linotype", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTitle.ForeColor = System.Drawing.Color.White;
+            this.lbTitle.Location = new System.Drawing.Point(0, 0);
+            this.lbTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbTitle.Name = "lbTitle";
+            this.lbTitle.Size = new System.Drawing.Size(363, 62);
+            this.lbTitle.TabIndex = 14;
+            this.lbTitle.Text = "Issue Breakdown Invoice";
+            this.lbTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnPrint);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.tbInvoiceNo);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(0, 62);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(363, 81);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Debit / Credit Note";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 85;
+            this.label2.Text = "DN/CN";
+            // 
+            // tbInvoiceNo
+            // 
+            this.tbInvoiceNo.Location = new System.Drawing.Point(62, 30);
+            this.tbInvoiceNo.MaxLength = 16;
+            this.tbInvoiceNo.Name = "tbInvoiceNo";
+            this.tbInvoiceNo.Size = new System.Drawing.Size(199, 20);
+            this.tbInvoiceNo.TabIndex = 84;
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(9)))), ((int)(((byte)(47)))));
+            this.btnPrint.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPrint.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrint.ForeColor = System.Drawing.Color.White;
+            this.btnPrint.Location = new System.Drawing.Point(278, 24);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(73, 30);
+            this.btnPrint.TabIndex = 169;
+            this.btnPrint.Text = "Print";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // BreakdownInvoice
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.ClientSize = new System.Drawing.Size(363, 143);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lbTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "BreakdownInvoice";
+            this.Text = "BreakdownInvoice";
+            this.Load += new System.EventHandler(this.BreakdownInvoice_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbTitle;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbInvoiceNo;
+        private cus_button btnPrint;
+    }
+}
---
+++ Testing/Forms/BreakdownInvoice.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Testing.Forms
+{
+    public partial class BreakdownInvoice : Form 
+    {
+        public string Username = "Admin";
+        public  DataTable dtInvoice;
+        CRUD crud = new CRUD();
+        public BreakdownInvoice()
+        {
+            InitializeComponent();
+        }
+
+        private void BreakdownInvoice_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (tbInvoiceNo.Text != "")
+                {
+                    dtInvoice = crud.ExecQuery("select p.*,rate from user_exchange_rate,view_print_invoice p where TRAN_DATE = ON_DATE and DEBIT_NOTE='" + tbInvoiceNo.Text.ToUpper() + "'");
+                    if (dtInvoice.Rows.Count == 0)
+                    {
+                        Msgbox.Show("Debit / Credit Note not found!");
+                    }
+                    else
+                    {
+
+                        dtInvoice.Columns.Add("EXCHANGE_RATE", typeof(System.String));
+                        dtInvoice.Columns.Add("TOTAL_FUND_KH", typeof(System.String));
+                        dtInvoice.Columns.Add("KH_NAME", typeof(System.String));
+                        dtInvoice.Columns.Add("KH_ADDR", typeof(System.String));
+
+                        InvoiceOption1 Invoice1 = new InvoiceOption1(this);
+                        Invoice1.dtInvoicefrm2 = this.dtInvoice;
+                        Invoice1.Show();
+                    }
+
+                }
+                else
+                {
+                    Msgbox.Show("Debit / Credit note is required");
+                }
+                
+
+            }catch(Exception ex){
+                Msgbox.Show(ex.Message);
+            }
+            
+
+        }
+
+        
+    }
+}
