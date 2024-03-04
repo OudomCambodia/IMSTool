c2d6d0253e3672c73c061626793459ba5397a7e0
Oudom <rothoudom@forteinsurance.com>
2024-03-04 10:38:14 +0700

    add Print Auto Label

---
+++ Testing/Forms/frmPrintAutoLabel.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Testing.Forms
+{
+    partial class frmPrintAutoLabel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrintAutoLabel));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtPolicyNo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rpvPrintAutoLabel = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnSearch);
+            this.panel1.Controls.Add(this.txtPolicyNo);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1031, 46);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(320, 9);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtPolicyNo
+            // 
+            this.txtPolicyNo.Location = new System.Drawing.Point(93, 9);
+            this.txtPolicyNo.MaxLength = 20;
+            this.txtPolicyNo.Name = "txtPolicyNo";
+            this.txtPolicyNo.Size = new System.Drawing.Size(221, 22);
+            this.txtPolicyNo.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Policy No:";
+            // 
+            // rpvPrintAutoLabel
+            // 
+            this.rpvPrintAutoLabel.ActiveViewIndex = -1;
+            this.rpvPrintAutoLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.rpvPrintAutoLabel.Cursor = System.Windows.Forms.Cursors.Default;
+            this.rpvPrintAutoLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rpvPrintAutoLabel.Location = new System.Drawing.Point(0, 46);
+            this.rpvPrintAutoLabel.Name = "rpvPrintAutoLabel";
+            this.rpvPrintAutoLabel.Size = new System.Drawing.Size(1031, 802);
+            this.rpvPrintAutoLabel.TabIndex = 1;
+            // 
+            // frmPrintAutoLabel
+            // 
+            this.AcceptButton = this.btnSearch;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1031, 848);
+            this.Controls.Add(this.rpvPrintAutoLabel);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmPrintAutoLabel";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Print Auto Label";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtPolicyNo;
+        private System.Windows.Forms.Label label1;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer rpvPrintAutoLabel;
+    }
+}
---
+++ Testing/Forms/frmPrintAutoLabel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Testing.Forms
+{
+    public partial class frmPrintAutoLabel : Form
+    {
+        CRUD crud = new CRUD();
+
+        public frmPrintAutoLabel()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (txtPolicyNo.TextLength < 20)
+            {
+                Msgbox.Show("Wrong policy no");
+                ActiveControl = txtPolicyNo;
+            }
+
+            var dtPrintAutoLabel = crud.ExecQuery("select * from VIEW_PRINT_AUTO_LABEL where POL_POLICY_NO = '" + txtPolicyNo.Text.ToUpper().Trim() + "'");
+
+            CrystalDecisions.CrystalReports.Engine.ReportClass report = new CrystalDecisions.CrystalReports.Engine.ReportClass();
+            report = new Reports.PrintAutoLabel();
+
+            report.SetDataSource(dtPrintAutoLabel);
+
+            rpvPrintAutoLabel.ReportSource = report;
+        }
+    }
+}
